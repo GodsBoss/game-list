a561539ec36778298321547b1155698f4e434198
Timo Reitz <godsboss@gmx.de>
2018-09-04 22:34:44 +0200

    Minimal working list rendering.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const PresApp = (data) => {
+  const rows = data.games.map(
+    (game) =>
+      <tr>
+        <td>{game.title}</td>
+      </tr>
+  )
+  return (
+    <table>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  )
 }
 
-export default App;
+const ContApp = connect((state) => state)(PresApp)
+
+export default ContApp;
