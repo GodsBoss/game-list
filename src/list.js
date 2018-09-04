051eedaf1bb8c356e998c0b81c66385d38ea7dda
Timo Reitz <godsboss@gmx.de>
2018-09-04 22:34:15 +0200

    Add minimal state.

---
+++ src/list.js
@@ -0,0 +1,15 @@
+function list(state = defaultState(), action) {
+  return state
+}
+
+function defaultState() {
+  return {
+    games: [
+      {
+        title: "Timining 1D"
+      }
+    ]
+  }
+}
+
+export default list
