d8d42589370c9233053d4541ebdd31e4bc836112
Timo Reitz <godsboss@gmx.de>
2018-09-09 16:33:24 +0200

    Expose configurable port.

--- yarn.sh
+++ yarn.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker run --rm -it -v `pwd`:/root/game-list --workdir /root/game-list node:8.11.4-alpine yarn $@
+if [ -n "${GAME_LIST_PORT}" ]
+then
+  PORT_ARG="-p${GAME_LIST_PORT}:3000"
+fi
+
+docker run --rm -it -v `pwd`:/root/game-list --workdir /root/game-list "${PORT_ARG}" node:8.11.4-alpine yarn $@
