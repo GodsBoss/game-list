248bd99bca5c4720d822e21feec5551be599fe6e
Timo Reitz <godsboss@gmx.de>
2018-09-09 16:34:16 +0200

    Add IDs to games.

--- src/reducers/state.js
+++ src/reducers/state.js
@@ -6,22 +6,27 @@ function defaultState() {
   return {
     games: [
       {
+        id: 1,
         title: "Timining 1D",
         url: "http://js.godsboss.org/timining-1d/"
       },
       {
+        id: 2,
         title: "Ten Seconds Treasure Hunt",
         url: "http://js.godsboss.org/ten-seconds-treasure-hunt/"
       },
       {
+        id: 3,
         title: "Torpedo 'em up!",
         url: "http://js.godsboss.org/torpedo-em-up/"
       },
       {
+        id: 4,
         title: "Shape/Shift",
         url: "http://js.godsboss.org/shape-shift/"
       },
       {
+        id: 5,
         title: "Planet Gaiardian",
         url: "http://js.godsboss.org/planet-gaiardian/"
       }
