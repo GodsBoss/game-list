e047413269f677d4bd75216bb2b4b9d1bc734039
Timo Reitz <godsboss@gmx.de>
2018-09-09 16:54:25 +0200

    Add descriptions to game data.

--- src/reducers/state.js
+++ src/reducers/state.js
@@ -9,6 +9,7 @@ function defaultState() {
         id: 1,
         game: {
           title: "Timining 1D",
+          description: "Gather wood, mine resources and craft the epic crown of nerdiness.",
           url: "http://js.godsboss.org/timining-1d/"
         },
         ld: {
@@ -20,6 +21,7 @@ function defaultState() {
         id: 2,
         game: {
           title: "Ten Seconds Treasure Hunt",
+          description: "Find a treasure... you have ten seconds.",
           url: "http://js.godsboss.org/ten-seconds-treasure-hunt/"
         },
         ld: {
@@ -31,6 +33,7 @@ function defaultState() {
         id: 3,
         game: {
           title: "Torpedo 'em up!",
+          description: "A simple horizontal shoot 'em up.",
           url: "http://js.godsboss.org/torpedo-em-up/"
         },
         ld: {
@@ -42,6 +45,7 @@ function defaultState() {
         id: 4,
         game: {
           title: "Shape/Shift",
+          description: "Shift shapes which also can shapeshift... or so.",
           url: "http://js.godsboss.org/shape-shift/"
         },
         ld: {
@@ -53,6 +57,7 @@ function defaultState() {
         id: 5,
         game: {
           title: "Planet Gaiardian",
+          description: "You are a planet gaiardian, destined to inspire growth.",
           url: "http://js.godsboss.org/planet-gaiardian/"
         },
         ld: {
