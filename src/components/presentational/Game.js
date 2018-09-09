297cd14be9f7bc1fd5a3835c9d422ec378a5156f
Timo Reitz <godsboss@gmx.de>
2018-09-09 17:38:03 +0200

    Show all information in game item.

--- src/components/presentational/Game.js
+++ src/components/presentational/Game.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Game = ({ game }) => (
-  <div>
-    {game.game.title}
+const Game = ({ game }) => {
+  const tagListItems = game.game.tags.map(
+    (tag) => <li class="tag">{tag}</li>
+  )
+  return <div>
+    <div class="title">
+      <a href={game.game.url}>{game.game.title}</a>
+    </div>
+    <div class="ld">
+      <a href={game.ld.url}>#{game.ld.no}</a>
+    </div>
+    <div class="description">
+      {game.game.description}
+    </div>
+    <div class="tags">
+      <ul>{tagListItems}</ul>
+    </div>
   </div>
-)
+}
 
 export default Game
